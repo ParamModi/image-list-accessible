b8d9a3d778c483fb40c41264690f9d401b2282d6
ParamModi <45822763+ParamModi@users.noreply.github.com>
2021-06-30 19:38:40 +0530

    Add files via upload
    Improved Accessibility

--- script.js
+++ script.js
@@ -6,14 +6,44 @@ let captionForImage = document.querySelector(".captionForImage");
 
 let displayIndex = 0;
 
-function truncatedString(titleOfImage) {
-	if (titleOfImage.length > 25) {
-		let prefix = titleOfImage.slice(0, 26);
-		prefix += "....";
-		prefix += titleOfImage.slice(titleOfImage.length - 9, titleOfImage.length);
-		titleOfImage = prefix;
-	}
-	return titleOfImage;
+// function truncatedString(titleOfImage) {
+// 	if (titleOfImage.length > 25) {
+// 		let prefix = titleOfImage.slice(0, 26);
+// 		prefix += "....";
+// 		prefix += titleOfImage.slice(titleOfImage.length - 9, titleOfImage.length);
+// 		titleOfImage = prefix;
+// 	}
+// 	return titleOfImage;
+// }
+
+function truncatedString() {
+	document.querySelectorAll("li").forEach((item, index) => {
+		let paragraph = item.querySelector("p");
+		let imageName = listOfImages[index]["title"];
+
+		if (paragraph.scrollWidth <= paragraph.clientWidth) return;
+
+		let low = 0,
+			high = imageName.length,
+			finalResult = "";
+
+		while (low < high) {
+			let mid = Math.floor((low + high) / 2.0);
+			let leftPart = Math.floor(mid / 2.0);
+			let rightPart = Math.ceil(mid / 2.0);
+			paragraph.innerHTML =
+				imageName.substr(0, leftPart) +
+				"..." +
+				imageName.substr(imageName.length - rightPart, rightPart);
+			if (item.scrollWidth <= item.clientWidth) {
+				finalResult = paragraph.textContent;
+				low = mid + 1;
+			} else {
+				high = mid - 1;
+			}
+		}
+		paragraph.innerHTML = finalResult;
+	});
 }
 
 listOfImages.forEach((image, index) => {
@@ -24,12 +54,20 @@ listOfImages.forEach((image, index) => {
 	imageEntry.src = image.source;
 	imageEntry.alt = image.alt;
 
-	imageName.innerHTML = truncatedString(image.title);
-	imageName.setAttribute("aria-label", image.title);
+	imageName.innerHTML = image.title;
+	// imageName.setAttribute("aria-label", image.title);
+
+	// imageName.setAttribute("tabindex", "-1");
 
 	listEntry.append(imageEntry);
 	listEntry.append(imageName);
+
+	listEntry.setAttribute("role", "tab");
+	listEntry.setAttribute("aria-controls", "mainDisplay");
 	listEntry.setAttribute("tabindex", "0");
+	listEntry.setAttribute("aria-selected", "false");
+	listEntry.setAttribute("id", image.title);
+
 	listEntry.addEventListener("click", () => {
 		showImage(index);
 	});
@@ -39,18 +77,33 @@ listOfImages.forEach((image, index) => {
 	mainList.append(listEntry);
 });
 
+truncatedString();
+window.addEventListener("resize", truncatedString);
+
 showImage(0);
 
 document.addEventListener("keydown", (event) => {
-	if (event.key == "ArrowUp" && displayIndex > 0) showImage(displayIndex - 1);
-	else if (event.key == "ArrowDown" && displayIndex < listOfImages.length - 1)
-		showImage(displayIndex + 1);
+	if (event.key == "ArrowUp") {
+		if (displayIndex == 0) showImage(listOfImages.length - 1);
+		else showImage(displayIndex - 1);
+	} else if (event.key == "ArrowDown")
+		showImage((displayIndex + 1) % listOfImages.length);
 });
 
 function showImage(newIndex) {
 	mainList.childNodes[displayIndex].classList.remove("active");
+	mainList.childNodes[displayIndex].setAttribute("aria-selected", "false");
+
 	mainList.childNodes[newIndex].classList.add("active");
+	mainList.childNodes[newIndex].setAttribute("aria-selected", "true");
 	mainList.childNodes[newIndex].focus();
+
+	document
+		.getElementById("mainDisplay")
+		.setAttribute(
+			"aria-labelledby",
+			mainList.childNodes[newIndex].getAttribute("id")
+		);
 	mainImage.src = listOfImages[newIndex].source;
 	mainImage.alt = listOfImages[newIndex].alt;
 	captionForImage.innerHTML = listOfImages[newIndex].title;
